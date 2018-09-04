ed441e87d7a75247a484542c6e39ef0588069023
Your Name <you@example.com>
2018-09-04 14:38:05 +0800

    Initial Commit

---
+++ GuidePageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuidePageDemo
+//
+//  Created by Cary on 2018/9/4.
+//  Copyright © 2018年 Cary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
