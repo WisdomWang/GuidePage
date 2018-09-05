e90fa338ba278ec3393749702e4465d3b9fac727
Your Name <you@example.com>
2018-09-05 11:32:14 +0800

    first commit

---
+++ GuidePageDemo/GuidePage.swift
@@ -0,0 +1,21 @@
+//
+//  GuidePage.swift
+//  GuidePageDemo
+//
+//  Created by Cary on 2018/9/4.
+//  Copyright © 2018年 Cary. All rights reserved.
+//
+
+import UIKit
+
+class GuidePage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
